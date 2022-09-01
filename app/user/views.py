8d05c1237b0c6eda0395712b62b1f8c5bf677662
patrickcmd <pwalukagga@gmail.com>
2022-09-01 12:01:03 +0300

    Implemented token API.

--- app/user/views.py
+++ app/user/views.py
@@ -2,11 +2,22 @@
 Views for the User API.
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import AuthTokenSerializer, UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
 
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new toke for the user."""
+
+    # Override serializer because default serializer behaviour for
+    # ObtainToken uses username and password
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
